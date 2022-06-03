8259c382dd6d15b32f2ec7196c6e7a7611b9db73
Leinerke Suarez <leinerke2000@gmail.com>
2022-06-03 07:09:17 -0500

    Add: * Principal Colors. * Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'TwitterChirp': 'TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+      'TwitterChirpExtendedHeavy': 'TwitterChirpExtendedHeavy, Verdana, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    },
+    extend: {
+      colors: {
+        'primary': '#1d9bf0',
+        'primary-h': '#1a8cd8',
+        'white-h': '#d2e3fc',
+        'transparent-h': 'rgba(29, 155, 240, .1)',
+        'border-white': '#333639',
+        'border-transparent': '#536471',
+      },
+    },
   },
   plugins: [],
 };
