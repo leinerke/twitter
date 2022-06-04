0a6ebc5361a675d8d13fa79b7e5664f63663d0e3
Leinerke Suarez <leinerke2000@gmail.com>
2022-06-04 00:42:12 -0500

    Add fortawesome.

--- src/components/01-atoms/button/Button.stories.tsx
+++ src/components/01-atoms/button/Button.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Button, ButtonModifier } from './Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default {
   title: '01 Atoms/Button',
@@ -9,6 +11,7 @@ export default {
 
 const Template: ComponentStory<typeof Button> = args => (
   <Button {...args}>
+    <FontAwesomeIcon icon={faTwitter} className="pr-2" />
     Button
   </Button>
 );
