223391d5b0ac0611fdacbcf94abdb0bccac83b82
Leinerke Suarez <leinerke2000@gmail.com>
2022-06-05 12:18:33 -0500

    Create Home.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app';
 import '@styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      '--doc-height',
+      `${window.innerHeight}px`,
+    );
+
+    window.addEventListener('resize', () => {
+      console.log('hola');
+      document.documentElement.style.setProperty(
+        '--doc-height',
+        `${window.innerHeight}px`,
+      );
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
